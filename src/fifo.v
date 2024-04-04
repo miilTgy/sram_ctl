10296f9cc024b14c90d329b528326e6ee850bf11
miils_server <tgy_scut2021@outlook.com>
2024-04-04 23:52:27 +0800

    Add full function for fifo.(ver. 1)

--- src/fifo.v
+++ src/fifo.v
@@ -6,27 +6,53 @@ module fifo #(
         // ports
         input                               rst,
         input                               clk,
-        input                               read,
+        input                               next_data,
         input                               wr_sop,
         input                               wr_eop,
         input                               wr_vld,
         input       [fifo_data_width-1:0]   wr_data,
-        output  reg                         sop,
-        output  reg                         eop,
-        output  reg                         vld,
-        output  reg [fifo_data_width-1:0]   out_data
+        output  reg                         ready, // ready=1 means there is data to be readed in fifo_buf.
+        output  reg                         overflow,
+        output  wire                        sop, // 这里用wire是因为这条线直接接在fifo_buf上，
+        output  wire                        eop, // 而fifo_buf是reg类型，所以没必要再套一层reg了。
+        output  wire                        vld,
+        output  wire[fifo_data_width-1:0]   out_data
     );
 
-    reg [fifo_data_width-1+3:0] fifo [fifo_num_of_priority-1:0];
+    reg [fifo_data_width-1+3:0] fifo_buf [fifo_num_of_priority-1:0];
+    reg [2:0] wptr, rptr;
+    reg working;
     integer i=32'b0;
 
     always @(posedge clk ) begin
         if (rst) begin
-            sop <= 1'b0; eop <= 1'b0; vld <= 1'b0;
+            wptr <= 3'b0; rptr <= 3'b0; working <= 1'b0;
             for (i = 32'b0; i < fifo_num_of_priority; i++) begin
-                fifo[i] <= fifo[i] ^ fifo[i];
+                fifo_buf[i] <= fifo_buf[i] ^ fifo_buf[i];
+            end
+        end else begin
+            // read out
+            if (ready) begin
+                if (!next_data) begin
+                    rptr <= rptr + 3'b1;
+                    if (wptr == rptr+3'b1) begin // if fifo_buf is empty
+                        ready <= 1'b0; // reset ready
+                    end
+                end
+            end
+            //write in
+            if (wr_sop) begin
+                working <= 1'b1;
+            end
+            if (working && wr_vld) begin
+                fifo_buf[wptr] <= {wr_sop, wr_eop, wr_vld, wr_data};
+                wptr <= wptr + 3'b1;
+                ready <= 1'b1;
+                overflow <= overflow | (rptr == (wptr+3'b1)); // if wptr catch up with rptr, overflow
             end
         end
     end
+
+    assign {sop, eop, vld, out_data} = fifo_buf[rptr];
 
 endmodule
