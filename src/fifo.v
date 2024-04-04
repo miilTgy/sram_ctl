adce28599fa8b4d970e511982e0c43d03d39bed9
miils_server <tgy_scut2021@outlook.com>
2024-04-04 22:34:11 +0800

    Add all ports for fifo and Add reset feature for fifo.

--- src/fifo.v
+++ src/fifo.v
@@ -1,10 +1,32 @@
 module fifo #(
-  // parameters
-  fifo_data_width = 256
-) (
-  // ports
-  input data_in,
-  output data_o
-);
-  
+        // parameters
+        fifo_data_width = 256,
+        fifo_num_of_priority = 8
+    ) (
+        // ports
+        input                               rst,
+        input                               clk,
+        input                               read,
+        input                               wr_sop,
+        input                               wr_eop,
+        input                               wr_vld,
+        input   [fifo_data_width-1:0]       wr_data,
+        output  reg                         sop,
+        output  reg                         eop,
+        output  reg                         vld,
+        output  [fifo_data_width-1:0]       out_data
+    );
+
+    reg [fifo_data_width-1+3:0] fifo [7:0];
+    integer i=32'b0;
+
+    always @(posedge clk ) begin
+        if (rst) begin
+            sop <= 1'b0; eop <= 1'b0; vld <= 1'b0;
+            for (i = 32'b0; i < 32'd8; i++) begin
+                fifo[i] <= fifo[i] ^ fifo[i];
+            end
+        end
+    end
+
 endmodule
