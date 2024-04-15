facc295430e287a036333d5bf51e649178528b39
miil <tgy_scut2021@outlook.com>
2024-04-15 20:26:06 +0800

    Complete arbiter_tb.

--- csrc/arbiter_tb.v
+++ csrc/arbiter_tb.v
@@ -1,7 +1,15 @@
 module arbiter_tb ();
 
     parameter arbiter_data_width = 256;
-    reg ckl, rst;
+    reg clk, rst, sp0_wrr1;
     reg [arbiter_data_width-1:0] data_in;
+
+    arbiter arbiter_test (
+        .rst        (rst        ),
+        .clk        (clk        ),
+        .sp0_wrr1   (sp0_wrr1   ),
+        .data_in    (data_in    ),
+        .data_out   (data_out   )
+    )
     
 endmodule
