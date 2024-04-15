08a3ed28d951c33f43e3f849240cb540b3ee0c20
miil <tgy_scut2021@outlook.com>
2024-04-15 21:26:10 +0800

    Move arbiter.v into ./src/arbiter dir.

---
+++ src/arbiter/arbiter.v
@@ -0,0 +1,30 @@
+module arbiter #(
+    // parameters
+    parameter arbiter_data_width = 256,
+    parameter num_of_ports       = 16;
+) (
+    // ports
+    input                                   rst,
+    input                                   clk,
+    input                                   sp0_wrr1,
+    input       [arbiter_data_width-1:0]    data_in     [num_of_ports-1:0],
+    output  reg [arbiter_data_width-1:0]    data_out
+);
+
+    always @(posedge clk ) begin
+        if (rst) begin
+            data_out <= {arbiter_data_width{1'b0}};
+        end else begin
+            case (sp0_wrr1)
+                1'b0: begin // *SP严格优先级
+                    // TODO
+                end
+                1'b1: begin // *WRR加权轮询调度
+                    // TODO
+                end
+                default: data_out <= data_out;
+            endcase
+        end
+    end
+    
+endmodule
