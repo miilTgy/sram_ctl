9c3c7089e115f4b6838756669f977c1618edee4c
miil <tgy_scut2021@outlook.com>
2024-04-15 20:17:43 +0800

    Add some ports in arbiter.

--- src/arbiter.v
+++ src/arbiter.v
@@ -1,7 +1,19 @@
 module arbiter #(
     // parameters
+    parameter arbiter_data_width = 256
 ) (
     // ports
+    input rst,
+    input clk,
+    input sp0_wrr1,
+    input [arbiter_data_width-1:0] data_in,
+    output reg [arbiter_data_width-1:0] data_out
 );
+
+    always @(posedge clk ) begin
+        if (rst) begin
+            data_out <= {arbiter_data_width{1'b0}};
+        end
+    end
     
 endmodule
