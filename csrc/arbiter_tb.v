ca0615c3c460460c10b048f5e1ad244de2bc0e9a
miil <tgy_scut2021@outlook.com>
2024-04-15 21:35:23 +0800

    Write testbench for arbiter aiming at testing the reset function.

--- csrc/arbiter_tb.v
+++ csrc/arbiter_tb.v
@@ -3,6 +3,7 @@ module arbiter_tb ();
     parameter arbiter_data_width = 256;
     reg clk, rst, sp0_wrr1;
     reg [arbiter_data_width-1:0] data_in;
+    wire [arbiter_data_width-1:0] data_out;
 
     arbiter arbiter_test (
         .rst        (rst        ),
@@ -10,6 +11,21 @@ module arbiter_tb ();
         .sp0_wrr1   (sp0_wrr1   ),
         .data_in    (data_in    ),
         .data_out   (data_out   )
-    )
+    );
+
+    always #1 clk = ~clk;
+    initial begin
+        $dumpfile("waveform.vcd");
+        $dumpvars;
+    end
+    initial begin
+        rst <= 0; sp0_wrr1 <= 0; clk <= 1'b1;
+        #10
+        rst <= 1;
+        #2
+        rst <= 0;
+        #10
+        $finish;
+    end
     
 endmodule
