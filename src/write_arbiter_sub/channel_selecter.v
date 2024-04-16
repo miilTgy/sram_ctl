bad4343a3e71540347108e9519bee64b0d40aea0
miil <tgy_scut2021@outlook.com>
2024-04-16 16:30:19 +0800

    Add necessary ports for channel_selecter(in write_arbiter_sub).

--- src/write_arbiter_sub/channel_selecter.v
+++ src/write_arbiter_sub/channel_selecter.v
@@ -4,11 +4,21 @@ module channel_selecter #(
     parameter arbiter_data_width = 256
 ) (
     // ports
-    input       [num_of_ports-1:0]          select,
-    input       [arbiter_data_width-1:0]    selected_data_in,
-    output      [arbiter_data_width-1:0]    selected_data_out,
-    output  reg [num_of_ports-1:0]          enabled
+    input                                                   clk,
+    input       [num_of_ports-1:0]                          select,
+    input       [(arbiter_data_width * num_of_ports)-1:0]   selected_data_in,
+    output      [arbiter_data_width-1:0]                    selected_data_out,
+    output  reg [num_of_ports-1:0]                          enabled
 );
+
+    wire    [arbiter_data_width-1:0]    datas   [num_of_ports-1:0];
+
+    genvar i;
+    generate
+        for (i = 0; i < num_of_ports; i = i + 1) begin
+            assign datas[i] = selected_data_in[(i + 1) * arbiter_data_width - 1 : i * arbiter_data_width];
+        end
+    endgenerate
 
     always @(posedge clk ) begin
         
