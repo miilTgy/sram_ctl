e144b8ab2e8df8d98ec1286a0a0a74a0369bc9d2
miil <tgy_scut2021@outlook.com>
2024-04-16 20:09:32 +0800

    Complete channel_selecter: add reset function.

--- src/write_arbiter_sub/channel_selecter.v
+++ src/write_arbiter_sub/channel_selecter.v
@@ -5,6 +5,7 @@ module channel_selecter #(
 ) (
     // ports
     input                                                   clk,
+    input                                                   rst,
     input                                                   enable,
     input       [3:0]                                       select,
     input       [(arbiter_data_width * num_of_ports)-1:0]   selected_data_in,
@@ -23,27 +24,31 @@ module channel_selecter #(
     endgenerate
 
     always @(posedge clk ) begin
-        // case (select)
-        //     4'd0:   selected_data_out <= datas[0];
-        //     4'd1:   selected_data_out <= datas[1];
-        //     4'd2:   selected_data_out <= datas[2];
-        //     4'd3:   selected_data_out <= datas[3];
-        //     4'd4:   selected_data_out <= datas[4];
-        //     4'd5:   selected_data_out <= datas[5];
-        //     4'd6:   selected_data_out <= datas[6];
-        //     4'd7:   selected_data_out <= datas[7];
-        //     4'd8:   selected_data_out <= datas[8];
-        //     4'd9:   selected_data_out <= datas[9];
-        //     4'd10:  selected_data_out <= datas[10];
-        //     4'd11:  selected_data_out <= datas[11];
-        //     4'd12:  selected_data_out <= datas[12];
-        //     4'd13:  selected_data_out <= datas[13];
-        //     4'd14:  selected_data_out <= datas[14];
-        //     4'd15:  selected_data_out <= datas[15];
-        //     default:selected_data_out <= selected_data_out;
-        // endcase
-        if (enable) begin
-            selected_data_out <= datas[select];
+        if (rst) begin
+            selected_data_out <= 0; enabled <= 0;
+        end else begin
+            // case (select)
+            //     4'd0:   selected_data_out <= datas[0];
+            //     4'd1:   selected_data_out <= datas[1];
+            //     4'd2:   selected_data_out <= datas[2];
+            //     4'd3:   selected_data_out <= datas[3];
+            //     4'd4:   selected_data_out <= datas[4];
+            //     4'd5:   selected_data_out <= datas[5];
+            //     4'd6:   selected_data_out <= datas[6];
+            //     4'd7:   selected_data_out <= datas[7];
+            //     4'd8:   selected_data_out <= datas[8];
+            //     4'd9:   selected_data_out <= datas[9];
+            //     4'd10:  selected_data_out <= datas[10];
+            //     4'd11:  selected_data_out <= datas[11];
+            //     4'd12:  selected_data_out <= datas[12];
+            //     4'd13:  selected_data_out <= datas[13];
+            //     4'd14:  selected_data_out <= datas[14];
+            //     4'd15:  selected_data_out <= datas[15];
+            //     default:selected_data_out <= selected_data_out;
+            // endcase
+            if (enable) begin
+                selected_data_out <= datas[select];
+            end
         end
     end
     
