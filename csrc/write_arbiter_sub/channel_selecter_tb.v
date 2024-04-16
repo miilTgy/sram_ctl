8d9e6bc63234fa81c87d94c680b3fa7d5853fbc6
miil <tgy_scut2021@outlook.com>
2024-04-16 20:10:03 +0800

    Add codes for channel_selecter_tb.

--- csrc/write_arbiter_sub/channel_selecter_tb.v
+++ csrc/write_arbiter_sub/channel_selecter_tb.v
@@ -1,5 +1,14 @@
 module channel_selecter_tb ();
     parameter arbiter_data_width = 256;
     parameter num_of_ports = 16;
-    reg clk, 
+    reg clk, rst, enable;
+    reg [arbiter_data_width-1:0] selected_data_in [num_of_ports];
+    wire [arbiter_data_width-1:0] selected_data_out;
+    wire enabled;
+
+    always clk = ~clk;
+    initial begin
+        clk = 1; rst = 0; enable = 0; selected_data_in = 0; 
+        #10
+    end
 endmodule
