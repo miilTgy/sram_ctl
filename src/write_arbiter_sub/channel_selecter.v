d43a2fa7fa06c055a4fe4daa0acc1cad9e464815
miil <tgy_scut2021@outlook.com>
2024-04-16 16:10:10 +0800

    Add some of ports for channel_selecter.

--- src/write_arbiter_sub/channel_selecter.v
+++ src/write_arbiter_sub/channel_selecter.v
@@ -1,7 +1,17 @@
 module channel_selecter #(
     // parameters
+    parameter num_of_ports = 16,
+    parameter arbiter_data_width = 256
 ) (
     // ports
+    input       [num_of_ports-1:0]          select,
+    input       [arbiter_data_width-1:0]    selected_data_in,
+    output      [arbiter_data_width-1:0]    selected_data_out,
+    output  reg [num_of_ports-1:0]          enabled
 );
+
+    always @(posedge clk ) begin
+        
+    end
     
 endmodule
