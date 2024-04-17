abe08db1f0d837f9ee13f0d279668b13093e5bd9
miil <tgy_scut2021@outlook.com>
2024-04-17 20:45:32 +0800

    Add necessary ports and port unzip for arbiter_core.

--- src/write_arbiter_sub/arbiter_core.v
+++ src/write_arbiter_sub/arbiter_core.v
@@ -1,5 +1,27 @@
-module arbiter_core (
-    // ports
+module arbiter_core # (
+    // parameters
+    parameter num_of_ports = 16
+) (
+    // port
+    input                                   sp0_wrr1,
+    input           [num_of_ports-1:0]      sop,
+    input           [num_of_ports*3-1:0]    priority_in,
+    output  reg     [3:0]                   select
 );
+            wire    [2:0]                   priority        [num_of_ports-1:0];
+    genvar i;
+    generate
+        for (i = 0; i < num_of_ports; i = i + 1) begin
+            assign priority[i] = priority_in[(i+1)*3-1:i*3];
+        end
+    endgenerate
+
+    always @(posedge clk ) begin
+        if (sp0_wrr1) begin // wrr
+            
+        end else begin      // sp
+            
+        end
+    end
     
 endmodule
