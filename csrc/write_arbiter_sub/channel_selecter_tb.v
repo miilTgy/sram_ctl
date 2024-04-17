f9af43b1e555484a203a60c4596a00b8d012f740
miil <tgy_scut2021@outlook.com>
2024-04-17 15:23:06 +0800

    Fixed syntex error for write_arbiter_sub/channel_selecter_tb.

--- csrc/write_arbiter_sub/channel_selecter_tb.v
+++ csrc/write_arbiter_sub/channel_selecter_tb.v
@@ -2,13 +2,17 @@ module channel_selecter_tb ();
     parameter arbiter_data_width = 256;
     parameter num_of_ports = 16;
     reg clk, rst, enable;
-    reg [arbiter_data_width-1:0] selected_data_in [num_of_ports];
+    reg [arbiter_data_width-1:0] selected_data_in [num_of_ports-1:0];
     wire [arbiter_data_width-1:0] selected_data_out;
     wire enabled;
+    integer i;
 
-    always clk = ~clk;
+    always #1 clk = ~clk;
     initial begin
-        clk = 1; rst = 0; enable = 0; selected_data_in = 0; 
-        #10
+        clk <= 1; rst <= 0; enable <= 0;
+        for (i = 0; i < num_of_ports; i = i + 1) begin
+            selected_data_in[i] <= 0;
+        end
+        #10;
     end
 endmodule
