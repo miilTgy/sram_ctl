ff5cabbaceff1414ba1cbe6680a8b4eecba71620
miil <tgy_scut2021@outlook.com>
2024-04-18 13:25:58 +0800

    Add necessary ports for arbiter_core_tb.

--- csrc/write_arbiter_sub/arbiter_core_tb.v
+++ csrc/write_arbiter_sub/arbiter_core_tb.v
@@ -1,3 +1,18 @@
 module arbiter_core_tb ();
+    parameter num_of_ports = 16;
+
+    reg clk, sp0_wrr1;
+    reg [num_of_ports-1:0] sop;
+    reg [2:0] priorities [num_of_ports-1:0];
+    wire [num_of_ports*3-1:0] priority_in;
+    wire [3:0] select;
+
+    // packup priorities
+    genvar i;
+    generate
+        for (i = 0; i<num_of_ports; i=i+1) begin
+            assign priority_in[(i+1)*3-1:i*3] = priorities[i];
+        end
+    endgenerate
     
 endmodule
