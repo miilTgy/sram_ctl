46d6e6c59d05ebb6575b5f3b5247881b52b793f8
miil <tgy_scut2021@outlook.com>
2024-04-21 18:15:13 +0800

    Add fifo's size.

--- src/fifo.v
+++ src/fifo.v
@@ -1,7 +1,8 @@
 module fifo #(
         // parameters
         parameter fifo_data_width      = 16,   // should be 256
-        parameter fifo_num_of_priority = 8
+        parameter fifo_num_of_priority = 8,
+        parameter fifo_length = 32
     ) (
         // ports
         input                               rst,
@@ -19,14 +20,14 @@ module fifo #(
         output  wire[fifo_data_width-1:0]   out_data
     );
 
-    reg [fifo_data_width-1+3:0] fifo_buf [fifo_num_of_priority-1:0];
-    reg [2:0] wptr, rptr;
+    reg [fifo_data_width-1+3:0] fifo_buf [fifo_length-1:0];
+    reg [4:0] wptr, rptr;
     reg working;
     integer i=32'b0;
 
     always @(posedge clk ) begin
         if (rst) begin
-            wptr <= 3'b0; rptr <= 3'b0; working <= 1'b0;
+            wptr <= 5'b0; rptr <= 5'b0; working <= 1'b0;
             for (i = 32'b0; i < fifo_num_of_priority; i++) begin
                 fifo_buf[i] <= fifo_buf[i] ^ fifo_buf[i];
             end
@@ -34,8 +35,8 @@ module fifo #(
             // read out
             if (ready) begin
                 if (next_data) begin
-                    rptr <= rptr + 3'b1;
-                    if (wptr == rptr+3'b1) begin // if fifo_buf empty
+                    rptr <= rptr + 5'b1;
+                    if (wptr == rptr+5'b1) begin // if fifo_buf empty
                         ready <= 1'b0; // reset ready
                     end
                 end
@@ -49,9 +50,9 @@ module fifo #(
             end
             if (working && wr_vld) begin
                 fifo_buf[wptr] <= {wr_sop, wr_eop, wr_vld, wr_data};
-                wptr <= wptr + 3'b1;
+                wptr <= wptr + 5'b1;
                 ready <= 1'b1;
-                overflow <= overflow | (rptr == (wptr+3'b1)); // if wptr catch up with rptr, overflow
+                overflow <= overflow | (rptr == (wptr+5'b1)); // if wptr catch up with rptr, overflow
             end
         end
     end
