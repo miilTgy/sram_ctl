7d4b0053d33d4ff18ab8de22d14b3793731af7ca
miil <tgy_scut2021@outlook.com>
2024-04-22 15:44:25 +0800

    Modified arbiter_core_tb: change variable name from sop to ready.

--- csrc/write_arbiter_sub/arbiter_core_tb.v
+++ csrc/write_arbiter_sub/arbiter_core_tb.v
@@ -2,7 +2,7 @@ module arbiter_core_tb ();
     parameter num_of_ports = 16;
 
     reg clk, sp0_wrr1, rst;
-    reg [num_of_ports-1:0] sop;
+    reg [num_of_ports-1:0] ready;
     reg [num_of_ports-1:0] eop;
     reg [2:0] priorities [num_of_ports-1:0];
     wire [num_of_ports*3-1:0] priority_in;
@@ -21,7 +21,7 @@ module arbiter_core_tb ();
         .clk                (clk            ),
         .rst                (rst            ),
         .sp0_wrr1           (sp0_wrr1       ),
-        .sop                (sop            ),
+        .ready              (ready          ),
         .eop                (eop            ),
         .priority_in        (priority_in    ),
         .select             (select         ),
@@ -40,7 +40,7 @@ module arbiter_core_tb ();
     integer j;
     initial begin
         clk <= 1; rst <= 0; sp0_wrr1 <= 0;
-        sop <= {num_of_ports{1'b0}};
+        ready <= {num_of_ports{1'b0}};
         eop <= {num_of_ports{1'b0}};
         for (j=0; j<num_of_ports; j=j+1) begin
             priorities[j] <= 3'b000;
@@ -51,11 +51,11 @@ module arbiter_core_tb ();
         rst <= 1'b0;
         #20;
         for (ii=0; ii<num_of_ports; ii=ii+1) begin
-            sop[ii] <= $random;
+            ready[ii] <= $random;
             priorities[ii] <= {$random}%8;
         end
         #4
-        sop[select] <= 1'b0;
+        ready[select] <= 1'b0;
         #20;
         eop[select] <= 1'b1;
         #2;
