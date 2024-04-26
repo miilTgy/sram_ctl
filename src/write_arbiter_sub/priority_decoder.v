b8290bb79730642265d059467553a9938955d5e2
miil <tgy_scut2021@outlook.com>
2024-04-26 21:51:46 +0800

    Add addition necessary ports for priority_decoder.

--- src/write_arbiter_sub/priority_decoder.v
+++ src/write_arbiter_sub/priority_decoder.v
@@ -6,12 +6,18 @@ module priority_decoder #(
 ) (
     // ports
     input                                               clk,
+    input                                               rst,
     input       [arbiter_data_width*num_of_ports-1:0]   priority_decoder_in,
     input       [num_of_ports-1:0]                      ready,
+    input       [num_of_ports-1:0]                      eop,
+    input       [3:0]                                   select,
     output  reg [num_of_ports*priority_width-1:0]       priority_out
 );
+
     wire [arbiter_data_width-1:0] priorities_tmp [num_of_ports-1:0];
     wire [priority_width-1:0] priorities [num_of_ports-1:0];
+
+    reg holding;
 
     genvar j;
     generate
@@ -21,12 +27,22 @@ module priority_decoder #(
         end
     endgenerate
 
+    // assign select_reg = select;
+
     integer i;
     always @(posedge clk ) begin
-        if (| ready) begin
-            for (i=0; i<num_of_ports; i=i+1) begin
-                // 这里运用了一个新语法：signal[i+:n]表示从i位开始往上取n位。
-                priority_out[i*priority_width+:priority_width] <= priorities[i];
+        if (rst) begin
+            holding <= 1'b0;
+        end else begin
+            if ((| ready) && (! holding)) begin
+                for (i=0; i<num_of_ports; i=i+1) begin
+                    // 这里运用了一个新语法：signal[i+:n]表示从i位开始往上取n位。
+                    priority_out[i*priority_width+:priority_width] <= priorities[i];
+                    holding <= 1'b1;
+                end
+            end else if (holding && eop[select]) begin
+                holding <= 1'b0;
+                priority_out <= {num_of_ports*priority_width{1'b0}};
             end
         end
     end
