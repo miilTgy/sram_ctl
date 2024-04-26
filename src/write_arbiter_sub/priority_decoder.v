1a34e9ca2392d9a150f1d3cce01f7f3a0d956c55
miil <tgy_scut2021@outlook.com>
2024-04-26 18:36:24 +0800

    Add ports and parameters for priority_decoder.

--- src/write_arbiter_sub/priority_decoder.v
+++ src/write_arbiter_sub/priority_decoder.v
@@ -1,7 +1,13 @@
 module priority_decoder #(
     // parameters
+    parameter arbiter_data_width = 256,
+    parameter num_of_ports       = 16,
+    parameter priority_width     = 3
 ) (
     // ports
+    input       [arbiter_data_width*num_of_ports-1:0]   priority_decoder_in,
+    input       [num_of_ports-1:0]                      ready,
+    output  reg [num_of_ports*priority_width-1:0]       priority_out
 );
     
 endmodule
