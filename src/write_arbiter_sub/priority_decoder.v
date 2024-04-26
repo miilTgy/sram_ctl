9031cf6bfdea3d9a08371ab90329eadd6f1596e9
miil <tgy_scut2021@outlook.com>
2024-04-26 20:14:37 +0800

    Add basic function for priority_decoder.

--- src/write_arbiter_sub/priority_decoder.v
+++ src/write_arbiter_sub/priority_decoder.v
@@ -5,9 +5,30 @@ module priority_decoder #(
     parameter priority_width     = 3
 ) (
     // ports
+    input                                               clk,
     input       [arbiter_data_width*num_of_ports-1:0]   priority_decoder_in,
     input       [num_of_ports-1:0]                      ready,
     output  reg [num_of_ports*priority_width-1:0]       priority_out
 );
-    
+    wire [arbiter_data_width-1:0] priorities_tmp [num_of_ports-1:0];
+    wire [priority_width-1:0] priorities [num_of_ports-1:0];
+
+    genvar j;
+    generate
+        for (j=0; j<num_of_ports; j=j+1) begin
+            assign priorities_tmp[j] = priority_decoder_in[(j+1)*arbiter_data_width-1:j*arbiter_data_width];
+            assign priorities[j] = priorities_tmp[j][6:4];
+        end
+    endgenerate
+
+    integer i;
+    always @(posedge clk ) begin
+        if (| ready) begin
+            for (i=0; i<num_of_ports; i=i+1) begin
+                // 这里运用了一个新语法：signal[i+:n]表示从i位开始往上取n位。
+                priority_out[i*priority_width+:priority_width] <= priorities[i];
+            end
+        end
+    end
+
 endmodule
