9edc03a9adb13a7d5f89a8c2132d8f11807409a5
miil <tgy_scut2021@outlook.com>
2024-04-27 17:05:12 +0800

    Add additional necessary ports for write_arbiter.

--- src/write_arbiter.v
+++ src/write_arbiter.v
@@ -13,17 +13,18 @@ module write_arbiter #(
     input           [num_of_ports-1:0]                          eop,
     input           [num_of_ports-1:0]                          vld,
     input   wire    [(num_of_ports * arbiter_data_width)-1:0]   data_in_p,
-    output  reg     [(arbiter_data_width)-1:0]                  data_out,
+    output  wire                                                busy,
+    output  reg     [(arbiter_data_width)-1:0]                  selected_data_out,
     output  reg     [num_of_ports-1:0]                          next_data
 );
 
-    wire    [arbiter_data_width-1:0]    data_in     [num_of_ports-1:0];
+    wire    [arbiter_data_width-1:0]    selected_data_in    [num_of_ports-1:0];
 
     // 压缩data_in_p端口
     genvar i;
     generate
         for (i = 0; i < num_of_ports; i = i + 1) begin
-            assign data_in[i] = data_in_p[(i + 1) * arbiter_data_width - 1 : i * arbiter_data_width];
+            assign data_in[i] = data_in_p[(i+1)*arbiter_data_width-1:i*arbiter_data_width];
         end
     endgenerate
 
