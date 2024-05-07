007a9214c73366b4fbdc80d17b8f05cf6194d704
miil <tgy_scut2021@outlook.com>
2024-05-07 21:53:28 +0800

    Add ports of write_arbiter for fifo_write_arbiter

--- csrc/fifo_write_arbiter.v
+++ csrc/fifo_write_arbiter.v
@@ -12,12 +12,22 @@ module fifo_write_arbiter ();
     reg rst;
     reg in_clk, clk;
     reg [num_of_ports-1:0] wr_sop, wr_eop, wr_wld;
-    reg [fifo_data_width-1:0] wr_data [num_of_ports-1:0];
+    reg [fifo_data_width-1:0] wr_data_unpack [num_of_ports-1:0];
+    reg [fifo_data_width*num_of_ports-1:0] wr_data;
+    wire [fifo_data_width*num_of_ports-1:0] out_data;
 
+    // ports for all
     wire [num_of_ports-1:0] next_data;
     wire [num_of_ports-1:0] ready;
     wire overflow [num_of_ports-1:0];
     wire [num_of_ports-1:0] sop, eop, vld;
-    wire [fifo_data_width-1:0] out_data [num_of_ports-1:0];
+    wire [fifo_data_width-1:0] between_data_unpack [num_of_ports-1:0]; // 接到fifo的out_data
+
+    // ports for write_arbiter
+    reg sp0_wrr1;
+    wire busy;
+    wire [arbiter_data_width-1:0] data_out_unpack [num_of_ports-1:0];
+    wire [arbiter_data_width*num_of_ports-1:0] data_out;
+    wire [fifo_data_width*num_of_ports-1:0] data_in_p;
 
 endmodule
