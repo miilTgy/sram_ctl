96bcb43765d6f8c1197a4a89a4206a0c4277adc2
miilTgy <142780211+miilTgy@users.noreply.github.com>
2024-05-10 17:31:53 +0800

    Add necessary ports ofr datasg module.

--- src/datasg.v
+++ src/datasg.v
@@ -1,7 +1,25 @@
 module datasg #(
     // parameters
+    // [x] Make parameters
+    parameter sg_data_width = 64,
+    parameter sg_address_width = 12,
+    parameter sg_des_width = 4,
+    parameter sg_priority_width = 3
 ) (
     // ports
+    // [.] Make ports
+    input                               rst,
+    input   [sg_data_width-1:0]         data_in,
+    output                              request,
+    output  [sg_priority_width-1:0]     wr_priority,
+    output  [sg_des_width-1:0]          des_port,
+    output  [sg_address_width-1:0]      address_write,
+    output  [sg_data_width-1:0]         data_write
 );
-    // [ ] Complete this
+    // [.] Complete this
+    always @(posedge clk ) begin
+        if (rst) begin
+            
+        end
+    end
 endmodule
