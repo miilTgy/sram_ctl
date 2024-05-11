768887b19a1a6a098c00fc65381b0cb599e26448
miil <142780211+miilTgy@users.noreply.github.com>
2024-05-11 23:57:52 +0800

    Add fully functional test for datasg_tb.

--- csrc/datasg_tb.v
+++ csrc/datasg_tb.v
@@ -1,5 +1,6 @@
 module datasg_tb ();
     // parameters
+    parameter num_of_ports = 16;
     parameter sg_data_width = 64;
     parameter sg_address_width = 12;
     parameter sg_des_width = 4;
@@ -8,6 +9,8 @@ module datasg_tb ();
     // ports
     reg rst, transfering, busy, clk;
     reg [sg_data_width-1:0] data_in;
+    reg [sg_address_width-1:0] address_in;
+    reg [num_of_ports-1:0]  eop;
     reg [sg_address_width-1:0] priority_in;
     reg [sg_des_width-1:0] des_port_in;
 
@@ -23,7 +26,9 @@ module datasg_tb ();
         .clk                    (clk),
         .transfering            (transfering),
         .busy                   (busy),
+        .eop                    (eop),
         .data_in                (data_in),
+        .address_in             (address_in),
         .priority_in            (priority_in),
         .des_port_in            (des_port_in),
         .request                (request),
@@ -32,6 +37,8 @@ module datasg_tb ();
         .address_write          (address_write),
         .data_write             (data_write)
     );
+
+    integer i;
 
     // clk
     always #1 clk = ~clk;
@@ -44,10 +51,31 @@ module datasg_tb ();
         clk <= 1; rst <= 0; busy <= 0;
         transfering <= 0; data_in <= 0;
         priority_in <= 0; des_port_in <= 0;
+        eop <= 0; address_in <= 0;
         #2;
         rst <= 1;
         #2;
         rst <= 0;
+        #2;
+        busy <= 1;
+        priority_in <= 6;
+        #2;
+        transfering <= 1;
+        des_port_in <= 9;
+        #2;
+        address_in <= $random;
+        for (i=0; i<5; i=i+1) begin
+            data_in <= {$random, $random};
+            if (i == 4) begin
+                eop[5] <= 1;
+            end
+            #2;
+        end
+        transfering <= 0;
+        busy <= 0;
+        eop <= 0;
+        data_in <= 0;
+        address_in <= 0;
         #4;
         $finish;
     end
