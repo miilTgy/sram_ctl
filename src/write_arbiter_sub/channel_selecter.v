1114895155a310564b528399745e5f7cd953e57e
miilTgy <142780211+miilTgy@users.noreply.github.com>
2024-05-11 13:29:27 +0800

    channel_selecter增加des_port通道

--- src/write_arbiter_sub/channel_selecter.v
+++ src/write_arbiter_sub/channel_selecter.v
@@ -1,7 +1,8 @@
 module channel_selecter #(
     // parameters
     parameter num_of_ports = 16,
-    parameter arbiter_data_width = 64
+    parameter arbiter_data_width = 64,
+    parameter des_port_width = 4
 ) (
     // ports
     input                                                   clk,
@@ -9,30 +10,41 @@ module channel_selecter #(
     input                                                   enable,
     input       [3:0]                                       select,
     input       [(arbiter_data_width * num_of_ports)-1:0]   selected_data_in,
+    input       [des_port_width*num_of_ports-1:0]           des_port_in,
     output  reg [arbiter_data_width-1:0]                    selected_data_out,
+    output  reg [des_port_width-1:0]                        des_port_out,
     output  reg [3:0]                                       enabled
 );
 
-    wire    [arbiter_data_width-1:0]    datas   [num_of_ports-1:0];
+    wire    [arbiter_data_width-1:0]    datas       [num_of_ports-1:0];
+    wire    [des_port_width-1:0]        des_ports   [num_of_ports-1:0];
+    reg                                 des_port_lock;
 
     // 解压缩selected_data_in端口
     genvar i;
     generate
         for (i = 0; i < num_of_ports; i = i + 1) begin
             assign datas[i] = selected_data_in[(i + 1) * arbiter_data_width - 1 : i * arbiter_data_width];
+            assign des_ports[i] = des_port_in[(i+1)*des_port_width-1:i*des_port_width];
         end
     endgenerate
 
     always @(posedge clk ) begin
         if (rst) begin
-            selected_data_out = 0; enabled = 0;
+            selected_data_out <= 0; enabled <= 0;
         end else begin
             if (enable) begin
-                selected_data_out = datas[select];
-                enabled = select;
+                selected_data_out <= datas[select];
+                if (!des_port_lock) begin
+                    des_port_out <= des_ports[select];
+                    des_port_lock <= 1'b1;                    
+                end
+                enabled <= select;
             end else begin
-                selected_data_out = {arbiter_data_width{1'b0}};
-                enabled = enabled;
+                selected_data_out <= {arbiter_data_width{1'b0}};
+                des_port_lock <= 1'b0;
+                des_port_out <= {des_port_width{1'b0}};
+                enabled <= 4'b0000;
             end
         end
     end
