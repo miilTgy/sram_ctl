449bc837d58f6c012fb4cb2937c6ed719cbbd6d9
ZhouDreams <zhou_dreams@foxmail.com>
2024-05-17 19:31:01 +0800

    增加了输出优先级队列

--- src/cache_manager_sup/chain_manager.v
+++ src/cache_manager_sup/chain_manager.v
@@ -1,4 +1,5 @@
 //用于记录内存空间的链表，寻找内存可用空间和内存回收
+//author: Dreams Zhou
 
 module chain_manager
 #(
@@ -7,35 +8,62 @@ module chain_manager
     parameter prev = 36, //[36:25]为prev
     parameter state = 24,//[24]为state
     parameter size = 23, //[23:12]为size
-    parameter sa = 11 //[11:0]为start_address
+    parameter sa = 11, //[11:0]为start_address
+
+    //端口优先级对应编号
+    parameter q0 = 0, //端口0为 [q0 +: 1] ～ [q0 +: 8]，以此类推 
+    parameter q1 = 8, //这样一来端口N的优先级M队列就是queue[N+M]，很文明
+    parameter q2 = 16,
+    parameter q3 = 24,
+    parameter q4 = 32,
+    parameter q5 = 40,
+    parameter q6 = 48,
+    parameter q7 = 56,
+    parameter q8 = 64,
+    parameter q9 = 72,
+    parameter q10 = 80,
+    parameter q11 = 88,
+    parameter q12 = 96,
+    parameter q13 = 104,
+    parameter q14 = 112,
+    parameter q15 = 120
 )
 (
     input rst,
     input clk,
 
+    //input declaration
+
     input wea, //write_enable
     input [7:0] w_size, //写入包长度
+    input [2:0] priority, //该数据包的优先级，0~7
+    input [3:0] dest_port, //该数据包的目标端口,0~15
     output reg [11:0] start_write_address, //写入起始地址
 
+    //output_declaration
+
     input rea, //read_enable
-    input [11:0] chain_id, //读出包对应的链表节点序号
+    input [8:0] chain_id, //读出包对应的链表节点序号
     output reg [11:0] start_read_address //读取起始地址
 
 
 );
 
-//----------declaration----------
-    reg [48:0] chain[units:0]; //双向链表，4096个单元，每个单元[11:0]为start_address，
-                                //[23:12]为size，[24]为state,[36:25]为prev,[48:37]为next
-
+//----------inner_declaration----------
+    reg [48:0] chain[units:0]; //双向链表，每个单元的数据定义参考parameter处注释
     reg [units:0] available;  //用于记录链表中某节点序号项是否被使用
     integer new_block; //指示新链表节点序号
+
+    //用于记录数据包输出顺序的队列，16个端口，每个端口8个优先级，每个优先级16个座位，每个座位记录一个chain_id
+    reg [8:0][15:0] queue[127:0]; //queue[端口+优先级][第几项]
+    reg      [3:0] queue_num[127:0]; //queue[端口+优先级]中目前项目数量
+
     //循环变量
-    integer initial_loop;
-    integer write_loop;
-    integer write_pointer;
-    integer deallocate_loop;
-    integer deallocate_pointer;
+    integer initial_loop;//rst过程的循环变量
+    integer write_loop; //写入过程的循环变量
+    integer write_pointer; //写入过程的当前链表节点指针
+    integer deallocate_loop; //内存回收过程的循环变量
+    integer deallocate_pointer; //内存回收过程的当前链表节点指针
 
 //----------initialization----------
     always @(posedge clk) begin
@@ -44,6 +72,8 @@ module chain_manager
                 chain[initial_loop] = 49'b0; //初始化整个链表
             for (initial_loop=0;initial_loop<=units;initial_loop=initial_loop+1)
                 available[initial_loop] = 1; //初始化available
+            for (initial_loop=0;initial_loop<=127;initial_loop=initial_loop+1)
+                    queue_num[initial_loop] = 4'b0;
 
             //初始化链表头chain[0]
             chain[0][sa-:12] = 1'b0; //start_address = 0
@@ -56,14 +86,14 @@ module chain_manager
         end
     end    
 
-//----------recording-new-package----------
+//----------allocate-new-space----------
     always @(posedge clk) begin
         if (wea) begin
             $display("wea is posedge");
             write_loop = 0;
             write_pointer = 0; //i为当前遍历到的链表节点编号
 
-            for(write_loop=0;write_loop<=units;write_loop=write_loop+1) begin //开始寻找可用内存块
+            for(write_loop=0;write_loop<units;write_loop=write_loop+1) begin //开始寻找可用内存块
 
                 $display("record loop = %d, pointer = %d, state = %d, size = %d",write_loop,write_pointer,chain[write_pointer][state],chain[write_pointer][size-:12]);
                 
@@ -74,6 +104,9 @@ module chain_manager
                     if(chain[write_pointer][size-:12] == w_size) begin 
                         chain[write_pointer][state] = 1; //如果需要分配的长度与内存块相同，则直接把state改为1
                         start_write_address = chain[write_pointer][sa-:12]; //输出起始地址
+
+                        queue[dest_port+priority][ queue_num[dest_port+priority] ] = write_pointer; //在当前队尾处写入该链表节点id
+                        queue_num[dest_port+priority] = queue_num[dest_port+priority] + 1; //该队列项目数量+1
 
                         write_loop = units+1;
                     end
@@ -89,6 +122,8 @@ module chain_manager
                         chain[write_pointer][sa-:12] = chain[new_block][sa-:12] + w_size; //旧块的start_address等于新块start_address+size 
 
                         start_write_address = chain[new_block][sa-:12]; //输出起始地址
+                        queue[dest_port+priority][ queue_num[dest_port+priority] ] = new_block; //在当前队尾处写入新链表节点id
+                        queue_num[dest_port+priority] = queue_num[dest_port+priority] + 1; //该队列项目数量+1
 
                         available[new_block] = 0; //刷新new_block
                         for(new_block = 0;available[new_block]==0;new_block=new_block+1); //从0开始寻找编号最小的未使用节点
@@ -114,11 +149,11 @@ module chain_manager
         end
     end
 
-//----------deallocate-memory----------
+//----------deallocate-nearby-free-space----------
     always @(negedge clk) begin
         deallocate_loop = 0;
         deallocate_pointer = 0;
-        for(deallocate_loop=0;deallocate_loop<=units;deallocate_loop=deallocate_loop+1) begin  //当chain[k]的next不等于null时继续
+        for(deallocate_loop=0;deallocate_loop<units;deallocate_loop=deallocate_loop+1) begin  //当chain[k]的next不等于null时继续
             
             if(chain[deallocate_pointer][24] == 0 && chain[ chain[deallocate_pointer][48:37] ][24] == 0 && available[deallocate_pointer] == 0 && available[ chain[deallocate_pointer][48:37] ] == 0) begin //当发现当前内存块和下一个内存块的state都为0，且两个块都正在被使用时，开始吞并
                 
@@ -131,4 +166,6 @@ module chain_manager
             deallocate_pointer = chain[deallocate_pointer][48:37]; //寻找下一个内存块节点序号
         end
     end
+
+    
 endmodule
