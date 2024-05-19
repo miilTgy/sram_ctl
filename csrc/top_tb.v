b8a0bdbcf1dc27f8952cfac5c47ca72b0225c760
miil <142780211+miilTgy@users.noreply.github.com>
2024-05-20 05:15:40 +0800

    testbench for top

--- csrc/top_tb.v
+++ csrc/top_tb.v
@@ -13,6 +13,7 @@ module top_tb ();
     parameter data_width = 64;
     parameter num_of_priorities = 8;
 
+reg clk, in_clk, rst;
 
 // ports
     reg sp0_wrr1;
@@ -26,6 +27,9 @@ module top_tb ();
 
 // instance
     top top_in (
+        .clk        (clk),
+        .in_clk     (in_clk),
+        .rst        (rst),
         .sp0_wrr1   (sp0_wrr1),
         .wr_sop     (wr_sop),
         .wr_eop     (wr_eop),
@@ -40,5 +44,42 @@ module top_tb ();
         .almost_full(almost_full)
     );
 
+    always #1 clk = ~clk;
+    always #16 in_clk = ~in_clk;
+    initial begin
+        $dumpfile("waveform.vcd");
+        $dumpvars;
+    end
+
+    initial begin
+        clk <= 1; in_clk <= 1; sp0_wrr1 <= 0;
+        rst <= 0; wr_sop <= 0; wr_eop <= 0;
+        wr_vld <= 0; wr_data <= 0; 
+        #2;
+        rst <= 1;
+        #2;
+        rst <= 0;
+        #4;
+        wr_sop[0] <= 1;
+        #32;
+        wr_sop[0] <= 0;
+        wr_vld[0] <= 1;
+        wr_data[3:0] <= 0;
+        wr_data[6:4] <= 0;
+        wr_data[14:7] <= 4;
+        #32;
+        wr_data[63:0] <= {$random, $random};
+        #32;
+        wr_data[63:0] <= {$random, $random};
+        #32;
+        wr_data[63:0] <= {$random, $random};
+        #32;
+        wr_data[63:0] <= {$random, $random};
+        #32;
+        wr_eop[63:0] <= 1;
+        wr_vld[63:0] <= 0;
+        #512;
+        $finish;
+    end
 
 endmodule
